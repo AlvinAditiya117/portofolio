df3a9b56205dbda54ec35629fc3bb8dad92c6bb6
AlvinAditiya117 <144310513+AlvinAditiya117@users.noreply.github.com>
2026-04-16 11:17:46 +0000

    Add new assets and styles for project setup
    - Added profile image and multiple project images to assets.
    - Included tech stack icons for various technologies.
    - Introduced new SVG icons for GitHub, Twitter, link, location, and office.
    - Created index.css for global styles and Tailwind CSS integration.
    - Set up index.js as the entry point for the React application.
    - Configured Tailwind CSS in tailwind.config.js with custom colors and font.

---
+++ src/Details.js
@@ -0,0 +1,183 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.svg";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/profile.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "ALVIN ADITIYA",
+  tagline: "D-IV Information Technology Student | Designer & Machine Learning Enthusiast",
+  location: "Lumajang, Jawa Timur",
+  img: profile,
+  about: `Mahasiswa aktif Program Studi D-IV Teknologi Informasi Politeknik Negeri Malang yang memiliki ketertarikan dalam bidang desain dan fotografi, terutama pada aspek visual storytelling dan user experience. Saya senang bereksperimen menggunakan Figma untuk membuat desain antarmuka yang fungsional dan menarik, serta mengembangkan minat di bidang machine learning menggunakan Python dan Google Colab. Bagi saya, desain dan teknologi adalah cara untuk menyalurkan kreativitas sekaligus memecahkan masalah dengan cara yang kreatif dan inovatif.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/alvin-aditiya",
+  github: "https://www.github.com/AlvinAditiya117",
+  twitter: "https://twitter.com/",
+  instagram: "https://www.instagram.com/",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Frontend Web Developer",
+    Company: `Company Name here`,
+    Location: "Bengaluru",
+    Type: "Full Time",
+    Duration: "Sep 2021 - Dec 2021",
+  },
+  {
+    Position: "Internship",
+    Company: `Company Name here`,
+    Location: "Bengaluru",
+    Type: "Internship",
+    Duration: "Sep 2021 - Dec 2021",
+  },
+  {
+    Position: "Internship",
+    Company: `Company Name here`,
+    Location: "Bengaluru",
+    Type: "Internship",
+    Duration: "Sep 2021 - Dec 2021",
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "D-IV Teknologi Informasi",
+    Company: "Politeknik Negeri Malang",
+    Location: "Kota Malang, Indonesia",
+    Type: "Full Time",
+    Duration: "Juli 2023 - Sekarang",
+  },
+  {
+    Position: "Semester 5 (IPK: 3.33 / 4.00)",
+    Company: "20 SKS",
+    Location: "Malang",
+    Type: "Full Time",
+    Duration: "2023 - Sekarang",
+  },
+  {
+    Position: "Multimedia",
+    Company: "SMKN 1 Pasirian",
+    Location: "Kota Lumajang, Indonesia",
+    Type: "Full Time",
+    Duration: "Juli 2020 - Mei 2023",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  sass: sass,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Project title 1",
+    image: projectImage1,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 2",
+    image: projectImage2,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 3",
+    image: projectImage3,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 4",
+    image: projectImage4,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 5",
+    image: projectImage5,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 6",
+    image: projectImage6,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "navin.arker@gmail.com",
+  phone: "+62 878 6035 6949",
+};
